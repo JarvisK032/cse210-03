82f1db3c474fa4a1798d6797272ef51f2fe3c166
JarvisK032 <97483860+JarvisK032@users.noreply.github.com>
2022-11-01 15:32:06 -0600

    Add files via upload

---
+++ Word.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace jumper
+{
+    public class HiddenWord
+    {
+        public string hiddenword;
+        List<char> answer = new List<char>();
+        public List<char> guess = new List<char>();
+
+        public HiddenWord()
+        {
+
+        }
+
+        public string pullWord()
+        {
+            List<string> words = new List<string>();
+            words.Add("giraffe");
+            words.Add("cantaloupe");
+            words.Add("prowler");
+            words.Add("hurricane");
+            words.Add("precipice");
+            words.Add("poltergeist");
+            words.Add("exponential");
+            words.Add("sagebrush");
+            words.Add("gazebo");
+            words.Add("liberty");
+
+            Random rand = new Random();
+            int randomIndex = rand.Next(0, words.Count);
+            string chosenWord = words[randomIndex];
+            return chosenWord;
+        }
+
+        public void listWord(string ripWord)
+        {
+            answer.AddRange(ripWord.ToLower());
+        }
+
+        public void createHiddenWord()
+        {
+            foreach (int i in answer)
+            {
+                guess.Add('_');
+            }
+        }
+
+        public void printGuess()
+        {
+            Console.WriteLine(string.Format("{0}", string.Join(" ", guess)));
+        }
+
+        public int compare(List<char> listPreviousGuesses, int numberOfGuesses)
+        {
+            for (int i = 0; i < answer.Count; i++)
+            {
+                if (listPreviousGuesses.Contains(answer[i]))
+                {
+                    guess[i] = answer[i];
+                }
+            }
+            if (answer.Contains(listPreviousGuesses[numberOfGuesses - 1]))
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+
+        }
+
+        public void printAnswer()
+        {
+            Console.WriteLine(string.Format("{0}", string.Join(" ", answer)));
+        }
+
+    };
+}
